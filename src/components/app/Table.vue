06c85374f66f1a4450a88ef7305972e115714313
Christian Medinilla <cmedinilla@intersysconsulting.com>
2017-10-25 14:22:36 -0500

    Add hook change

--- src/components/app/Table.vue
+++ src/components/app/Table.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="example-container" class="wrapper">
+      <br/>
       <HotTable :root="root" :settings="hotSettings"></HotTable>
   </div>
 </template>
@@ -9,20 +10,30 @@
   import HotTable from 'vue-handsontable-official';
   import Vue from 'vue';
 
+  const changeFunc = function(changes, source, col, row){
+    console.log(changes, source, col, row);
+  }
+
   let dataObject = [
   ["", "", "", "", ""]
   ];
 
-  let culumnHeaders = ['Profile','Level','Zone','Cost p/hour', 'Weekly Hours'];
+  const colWidthsConf = [150, 150, 150, 150,150, 150];
 
-  let columnsConf = [
+  const culumnHeaders = ['Profile','Level','Zone','Cost p/hour', 'Month Hours', 'Montly COST'];
+  const contextMenuConf = ['row_above', 'row_below', 'remove_row'];
+
+  let profiles = ['.Net', 'Big Data', 'Javascript UI', 'DBA'];
+  let levels = ['Junior', 'Intermediate', 'Senior', 'Master'];
+
+  const columnsConf = [
       { 
         type: 'dropdown',
-        source: ['.Net', 'Big Data', 'Javascript UI', 'DBA']
+        source: profiles
       },
       {
-      type: 'dropdown',
-        source: ['Junior', 'Intermediate', 'Senior', 'Master']
+        type: 'dropdown',
+        source: levels
       },
       {
         type: 'dropdown',
@@ -32,12 +43,37 @@
         type: 'numeric',
         format: '$0,0.00',
         language: 'en-US', // this is the default locale, set up for USD
-        allowEmpty: false
+        allowEmpty: false,
+        readOnly: true
       },
       {
         type: 'numeric'
+      },
+      {
+        type: 'numeric',
+        format: '$0,0.00',
+        language: 'en-US', // this is the default locale, set up for USD
+        allowEmpty: false,
+        readOnly: true
       }
     ] 
+
+  const ChangeHook = function (changes, source) {
+    if (changes === null)
+      return;
+
+
+
+      console.log(changes, source);
+        const row = changes[0][0];
+        const col = changes[0][1];
+        const oldVal = changes[0][2];
+        const newVal = changes[0][3];
+        if (col === 0) {
+        console.log(row,col);
+        if (newVal === "Big Data") { levels = ['Master','Senior']; console.log(levels); }
+      }
+    }
 
   export default {
     data: function() {
@@ -46,7 +82,10 @@
         hotSettings: {
           data: dataObject,
           colHeaders: culumnHeaders,
-          columns : columnsConf
+          columns : columnsConf,
+          contextMenu: contextMenuConf,
+          colWidths: colWidthsConf,
+          afterChange: ChangeHook
         }
       };
     },
@@ -58,7 +97,7 @@
 </script>
 <style>
   #test-hot {
-    width: 600px;
+    width: 1100px;
     height: 400px;
     overflow: hidden;
   }
