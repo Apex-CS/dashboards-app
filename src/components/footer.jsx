13a3ba7114499a69e01c2235659ff90157929039
el hombre de blanco <dev.alanparra@gmail.com>
2018-02-02 17:03:48 -0600

    changes in footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,22 +1,22 @@
 import React, { Component } from 'react'
 import '../assets/css/footer.css';
-//import { Footer } from 'react-materialize'
 
 class Footer extends Component {
     render() {
         return (
-            <div className="footer">
-                <div>
+            <div>
+                <div className="footer">
                     <ul className="menu">
                         <li><a className="grey-text text-lighten-3" target="_blank" href="https://www.intersysconsulting.com/company/">Company</a></li>
                         <li><a className="grey-text text-lighten-3" target="_blank" href="https://www.intersysconsulting.com/services/">Services</a></li>
-                        <li><a className="grey-text text-lighten-3" target="_blank" target="_blank" href="https://www.intersysconsulting.com/resources/">Resources</a></li>
-                        <li><a className="grey-text text-lighten-3" target="_blank" href="https://www.intersysconsulting.com/careers/">Carrers at Intersys</a></li>
+                        <li><a className="grey-text text-lighten-3" target="_blank" href="https://www.intersysconsulting.com/resources/">Resources</a></li>
+                        <li><a className="grey-text text-lighten-3" target="_blank" href="https://www.intersysconsulting.com/careers/">Careers at Intersys</a></li>
                         <li><a className="grey-text text-lighten-3" target="_blank" href="https://www.intersysconsulting.com/team-portal/">Team Portal</a></li>
                     </ul>
-                    <div className="copyright">
-                        &#169; Copyright Intersys Consulting
-                    </div>
+                    
+                </div>
+                <div className="copyright">
+                    &#169; 2018 Intersys Consulting, Inc. All Rights Reserved
                 </div>
             </div>
         );
