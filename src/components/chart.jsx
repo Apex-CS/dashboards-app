5a88e23cc97ddacfb5ef6b738248afcfdd3fe049
edgar-munoz <kontroll90@gmail.com>
2018-02-08 16:52:57 -0600

    connect props and graphics

--- src/components/chart.jsx
+++ src/components/chart.jsx
@@ -3,8 +3,6 @@ import React, { Component } from 'react';
 import '../assets/css/chart.css';
 import {
   Button,
-  Dropdown,
-  NavItem,
   Collection,
   CollectionItem
 } from 'react-materialize';
@@ -70,21 +68,8 @@ class Chart extends Component {
     ]
   };
   CHARTS = ['line', 'bar', 'area', 'gradient', 'dot'];
-
-  chartOptions = _.map(this.CHARTS, chart => (
-    <NavItem
-      key={chart}
-      onClick={e => {
-        e.preventDefault();
-        this.setState({ typeOfChart: chart });
-      }}
-    >
-      {chart} chart
-    </NavItem>
-  ));
-
   renderChart() {
-    if (this.state.typeOfChart === 'line') {
+    if (this.props.chartType === 'line') {
       return [
         <LineSeries
           key="one"
@@ -100,7 +85,8 @@ class Chart extends Component {
         />
       ];
     }
-    if (this.state.typeOfChart === 'bar') {
+
+    if (this.props.chartType === 'bar') {
       return [
         <VerticalBarSeries
           key="one"
@@ -114,7 +100,7 @@ class Chart extends Component {
         />
       ];
     }
-    if (this.state.typeOfChart === 'area') {
+    if (this.props.chartType === 'area') {
       return [
         <AreaSeries
           key="one"
@@ -128,7 +114,7 @@ class Chart extends Component {
         />
       ];
     }
-    if (this.state.typeOfChart === 'gradient') {
+    if (this.props.chartType === 'gradient') {
       return [
         <AreaSeries
           key="one"
@@ -161,9 +147,6 @@ class Chart extends Component {
     const { lastDrawLocation } = this.state;
     return (
       <div>
-        <Dropdown trigger={<Button>Select the type of chart!</Button>}>
-          {this.chartOptions}
-        </Dropdown>
         <FlexibleWidthXYPlot
           height={500}
           animation
