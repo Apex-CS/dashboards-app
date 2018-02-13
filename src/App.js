b27afcd17e6664e1e0a84de80266da024cae793f
edgar-munoz <kontroll90@gmail.com>
2018-02-13 17:14:42 -0600

    sidebar binding values

--- src/App.js
+++ src/App.js
@@ -13,21 +13,36 @@ class App extends Component {
     super(props);
     this.state = {
       typeOfChart: "line",
-      initMonth: "January",
-      initYear: "2017",
-      endMonth: "December",
-      endYear: "2017",
+      rangeOfValues: {
+        initMonth: "January",
+        initYear: "2017",
+        endMonth: "December",
+        endYear: "2017"
+      }
     };
     this.onChartTypeChange = this.onChartTypeChange.bind(this);
-    this.onRangeSelection = this.onRangeSelection.bind(this);
+    this.onValueChange = this.onValueChange.bind(this);
   }
 
   onChartTypeChange(newChartType){
     this.setState({typeOfChart: newChartType});
   }
   
-  onRangeSelection () {
-
+  onValueChange (newValue,typeOfValue) {
+    switch(typeOfValue) {
+      case 'initMonth':
+        this.state.rangeOfValues.initMonth = newValue
+        break;
+      case 'initYear':
+        this.state.rangeOfValues.initYear = newValue
+        break;
+      case 'endMonth':
+        this.state.rangeOfValues.endMonth = newValue
+        break;
+      case 'endYear':
+        this.state.rangeOfValues.endYear = newValue
+        break;
+    }
   }
   
   render() {
@@ -40,10 +55,10 @@ class App extends Component {
         </Row>
         <Row>
           <Col s={12} m={3}>
-            <SideMenu onChartTypeChange={this.onChartTypeChange} onRangeSelection={this.onRangeSelection}/>
+            <SideMenu onChartTypeChange={this.onChartTypeChange} rangeofValues={this.state.rangeOfValues} onValueChange={this.onValueChange}/>
           </Col>
           <Col s={12} m={9}>
-            <Chart chartType={this.state.typeOfChart} />
+            <Chart chartType={this.state.typeOfChart} onValueChange={this.onValueChange}/>
           </Col>
         </Row>
         <Row>
--- src/components/sidenav.jsx
+++ src/components/sidenav.jsx
@@ -26,10 +26,39 @@ export default class SideMenu extends Component {
         e.preventDefault();
         this.props.onChartTypeChange(chartType);
     }
+    
     rangeButtonsHandler(e,valuesRange) {
         e.preventDefault();
-        console.log(valuesRange, "c");
-        this.props.onRangeSelection(valuesRange);
+        console.log(this.props.rangeofValues)
+
+        if(valuesRange === "m3"){
+            console.log(valuesRange);
+            this.initMonth("February")
+        } 
+        else if(valuesRange === "m6"){
+            console.log(valuesRange);
+        }
+        else if(valuesRange === "y1"){
+            console.log(valuesRange);
+        }
+    }
+
+    initMonth(initialMonth) {
+        this.props.onValueChange(initialMonth,"initMonth");
+        console.log(this.props.rangeofValues.initMonth);
+    }
+
+    initYear(initialYear) {
+        this.props.onValueChange(initialYear,"initYear");
+        console.log(this.props.rangeofValues.initYear);
+    }
+    finalMonth(finalMonth) {
+        this.props.onValueChange(finalMonth,"endMonth");
+        console.log(this.props.rangeofValues.endMonth);
+    }
+    finalYear(finalYear) {
+        this.props.onValueChange(finalYear,"endYear");
+        console.log(this.props.rangeofValues.endYear);
     }
 
     render() {
@@ -47,12 +76,12 @@ export default class SideMenu extends Component {
             'November',
             'December'
         ];
-        const monthOptionList = months.map(function(month) {
+        const monthOptionList = months.map(month => {
             return <option value={month} key={month}>{month}</option>
         });
-
+        
         const years = [2015,2016,2017];
-        const yearsOptionList = years.map(function(year) {
+        const yearsOptionList = years.map(year => {
             return <option value={year} key={year}>{year}</option>
         });
 
@@ -74,25 +103,22 @@ export default class SideMenu extends Component {
                         </Col>
                     </Row>
                     <Row className="buttonsRange">
-                        <Button s={6} waves='light' value="3m" onClick={e => console.log(e,"m3")}>3m<Icon left>tune</Icon></Button>
-                        <Button s={6} waves='light' value="6m">6m<Icon left>tune</Icon></Button>
-                        <Button s={6} waves='light' value="1y">1y<Icon left>tune</Icon></Button>
                         
-                        <Collapsible s={12}>
-                            <CollapsibleItem header={<Button s={12} m={6} waves='light'>Other Values<Icon left>tune</Icon></Button>}>
-                                <Col s={12}>
-                                    <h6 s={12} l={6} className="rangeValues">Initial</h6>
-                                </Col>
-                                <Input s={6}  className="selector" type='select' label="Month">{monthOptionList}</Input>
-                                <Input s={6} type='select' label="Year">{yearsOptionList}</Input>
+                        <Col s={12}>
+                            <h6 s={12} l={6} className="rangeValues">Initial</h6>
+                        </Col>
+                        <Input s={6}  className="selector" type='select' label="Month" onChange={ e => this.initMonth(e.target.value)}>{monthOptionList}</Input>
+                        <Input s={6} type='select' label="Year" onChange={ e => this.initYear(e.target.value)}>{yearsOptionList}</Input>
 
-                                <Col s={12}>
-                                    <h6 s={12} l={6} className="rangeValues">Final</h6>
-                                </Col>
-                                <Input s={6} className="selector" type='select' label="Month">{monthOptionList}</Input>
-                                <Input s={6} type='select' label="Year">{yearsOptionList}</Input>
-                            </CollapsibleItem>
-                        </Collapsible>
+                        <Col s={12}>
+                            <h6 s={12} l={6} className="rangeValues">Final</h6>
+                        </Col>
+                        <Input s={6} className="selector" type='select' label="Month" onChange={ e => this.finalMonth(e.target.value)}>{monthOptionList}</Input>
+                        <Input s={6} type='select' label="Year" onChange={ e => this.finalYear(e.target.value)}>{yearsOptionList}</Input>
+                        
+                        <Button s={6} waves='light' onClick={e => this.rangeButtonsHandler(e,"m3")}>3m<Icon left>tune</Icon></Button>
+                        <Button s={6} waves='light' onClick={e => this.rangeButtonsHandler(e,"m6")}>6m<Icon left>tune</Icon></Button>
+                        <Button s={6} waves='light' onClick={e => this.rangeButtonsHandler(e,"y1")}>1y<Icon left>tune</Icon></Button>
                     </Row>
                     <Row className="chartType">
                         <Col s={12}>
