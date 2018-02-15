d3e6f12d1ff9a7234beac8f4774aee99842f744e
edgar-munoz <kontroll90@gmail.com>
2018-02-15 12:32:11 -0600

    style in header + new values file

---
+++ src/components/charts/values.js
@@ -0,0 +1,21 @@
+
+var data = [
+   {
+    month: "January",
+    year: 2015,
+    income: 30.5,
+    outcome: 22.8,
+    indexYear: 0,
+    indexMonth: 0,
+   },
+   {
+    month: "February",
+    year: 2015,
+    income: 22.7,
+    outcome: 19.3,
+    indexYear: 0,
+    indexMonth: 1,
+   },
+];
+
+export data;
