91760307fa0b12f9b548d9ab1e7c6bd20474de86
edgar-munoz <kontroll90@gmail.com>
2018-02-27 16:29:28 -0600

    pie chart plotting values

--- src/components/NetRevenueChart.jsx
+++ src/components/NetRevenueChart.jsx
@@ -8,17 +8,70 @@ import {
   FlexibleWidthXYPlot
 } from 'react-vis';
 import { Colors } from '../assets/theme';
-import { SERVERS } from './charts/values2';
+import { NETREVENUE } from './charts/values2';
 
 export default class SimpleRadialChart extends Component {
   state = {
     inheritProps: {},
-    values: false
+    netRevenue: {
+      title: 'Net Revenue',
+      disabled: false,
+      data: []
+    },
+    value: false
+  };
+
+  constructor(props) {
+    super(props);
+    this.buildDataset(this.props.rangeOfValues);
+  }
+
+  componentWillReceiveProps(newProps) {
+    this.buildDataset(newProps.rangeOfValues);
+  }
+
+  buildDataset(newData) {
+    const { netRevenue } = this.state;
+    const initial = NETREVENUE.findIndex(
+      element =>
+        element.year === newData.initYear && element.month === newData.initMonth
+    );
+    const end = NETREVENUE.findIndex(
+      element =>
+        element.year === newData.endYear && element.month === newData.endMonth
+    );
+    const range = NETREVENUE.slice(initial, end + 1);
+    
+    netRevenue.data = range.reduce((total,value) => {
+        
+        return {
+            servers: total.servers + value.servers,
+            storage: total.storage + value.storage,
+            networking: total.networking + value.networking,
+            services: total.services + value.services,
+            finalConsumer: total.finalConsumer + value.finalConsumer,
+            software: total.software + value.software,
+        };
+
+    });
+
+    for (var key in netRevenue.data) {
+        netRevenue.data[key] = netRevenue.data[key] / range.length;
+    }
+    
+    this.setState({ netRevenue: netRevenue });
+    //console.log(this.state);
+  }
+  
+  assignProps(props) {
+    this.setState({ inheritProps: props });
   }
 
 
   render() {
-    const {value} = this.state;
+    const {netRevenue, value} = this.state;
+    console.log(netRevenue.data);
+    const dataValues = Object.keys(netRevenue.data).map( key => ({theta: netRevenue.data[key]}) );
     return (
         <Row className="pieChartBox">
             <Col s={12}>
@@ -27,14 +80,9 @@ export default class SimpleRadialChart extends Component {
             <RadialChart
                 className={'pieChart'}
                 innerRadius={0}
-                getAngle={d => d.theta}
-                data={[
-                {theta: 2, className: 'custom-class'},
-                {theta: 6},
-                {theta: 2},
-                {theta: 3},
-                {theta: 1}
-                ]}
+                getAngle={
+                    d => d.theta}
+                data={dataValues}
                 onValueMouseOver={v => this.setState({value: v})}
                 onSeriesMouseOut={v => this.setState({value: false})}
                 height={350}
