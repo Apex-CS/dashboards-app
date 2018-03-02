7bb7c86323a002e2fffa3827ed20b245ffc9aa9c
dbarriga <dbarriga@intersysconsulting.com>
2018-03-02 15:57:19 -0600

    adaptive component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Footer from './components/footer.jsx';
 import Chart from './components/chart.jsx';
 import ProfitChart from './components/ProfitChart.jsx';
 import SimpleRadialChart from './components/NetRevenueChart.jsx';
+import Adaptive from './components/Adaptive';
 import { Row, Col } from 'react-materialize';
 import Help_Page from './components/help_page';
 import './assets/css/main.css';
@@ -17,122 +18,149 @@ import '../node_modules/react-vis/dist/style.css';
 import Help_Title from './components/help_title';
 import 'primereact/resources/primereact.min.css';
 
-
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      typeOfChart: "line",
+      typeOfChart: 'line',
       rangeOfValues: {
-        initMonth: "February",
+        initMonth: 'February',
         initYear: 2017,
-        endMonth: "January",
+        endMonth: 'January',
         endYear: 2018
       },
       profit: false,
-      revenue: false
+      revenue: false,
+      screenSize: ''
     };
     this.onDashboardTypeChange = this.onDashboardTypeChange.bind(this);
     this.onValueChange = this.onValueChange.bind(this);
     this.showChart = this.showChart.bind(this);
   }
 
+  updateDimensions() {
+    const {screenSize} = this.state;
+    let size = this.screenSize;
+    switch (true) {
+      case window.innerWidth <= 480:
+        size = 'xxs';
+        break;
+      case window.innerWidth < 768:
+        size = 'xs';
+        break;
+      case window.innerWidth < 992:
+        size = 's';
+        break;
+      case window.innerWidth < 1200:
+        size = 'm';
+        break;
+      case window.innerWidth < 1600:
+        size = 'l';
+        break;
+      case window.innerWidth >= 1600: 
+        size = 'xl';
+        break;
+    }
+    if (size !== screenSize){
+      this.setState({screenSize: size});
+    }
+  }
+
+  componentDidMount() {
+    this.updateDimensions();
+    window.addEventListener('resize', this.updateDimensions.bind(this));
+  }
+
+  componentWillMount() {
+    window.removeEventListener('resize', this.updateDimensions.bind(this));
+  }
+
   onDashboardTypeChange(newChartType) {
     this.setState({ typeOfChart: newChartType });
   }
-  
-  onValueChange (newValue, typeOfValue) {
-    const {rangeOfValues} = this.state;
+
+  onValueChange(newValue, typeOfValue) {
+    const { rangeOfValues } = this.state;
     const range = rangeOfValues;
-    switch(typeOfValue) {
+    switch (typeOfValue) {
       case 'initMonth':
-        range.initMonth = newValue
+        range.initMonth = newValue;
         break;
       case 'initYear':
-        range.initYear = newValue
+        range.initYear = newValue;
         break;
       case 'endMonth':
-        range.endMonth = newValue
+        range.endMonth = newValue;
         break;
       case 'endYear':
-        range.endYear = newValue
+        range.endYear = newValue;
         break;
     }
-    this.setState({rangeofValues: range});
+    this.setState({ rangeofValues: range });
   }
 
-  showChart (chart, election) {
-    this.setState({[chart]: election});
+  showChart(chart, election) {
+    this.setState({ [chart]: election });
   }
   render() {
-    const {profit, revenue} = this.state;
+    const { profit, revenue, screenSize } = this.state;
     return (
       <div>
         <BrowserRouter>
           <div>
-            <Route exact path="/" render={(props) => (
-              <div>
-                <Row>
-                  <Col s={12}>
-                    <Header />
-                  </Col>
-                </Row>
-                <Row>
-                  <Col s={12}>
-                    <PitchHeader />
-                  </Col>
-                </Row>
-                <Row>
-                  <Col s={12}>
-                    <Features />
-                  </Col>
-                </Row>
-              </div>
-            )}/>
-            <Route exact path="/dashboards" render={(props) => (
-              <div>
-                <Row>
-                  <Col s={12}>
-                    <HeaderGraphics onDashboardTypeChange={this.onDashboardTypeChange}/>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col s={12} m={3}>
-                    <SideMenu onDashboardTypeChange={this.onDashboardTypeChange} rangeofValues={this.state.rangeOfValues} onValueChange={this.onValueChange} showChart={this.showChart}/>
-                  </Col>
-                  <Col s={12} m={9}>
-                    <Chart chartType={this.state.typeOfChart} rangeOfValues={this.state.rangeOfValues} />
-                  </Col>
-                
-                {profit && <Col s={12} m={9} >
-                    <ProfitChart rangeOfValues={this.state.rangeOfValues} />
-                  </Col>
-                }
-                { revenue && 
-                  <Col s={12} m={9} offset="m3">
-                  <SimpleRadialChart rangeOfValues={this.state.rangeOfValues} />
-                  </Col>
-               }
-               </Row>
-              </div>
-            )}/>
-            <Route exact path="/help" render={(props) => (
-              <div>
-                <Row>
-                  <Col s={12}>
-                    <Header />
-                  </Col>
-                </Row>
-                <Row>
-                  <Col s={12}><Help_Title /></Col>
-                </Row>
-                <Row>
-                  <Col s={12}>
-                    <Help_Page onDashboardTypeChange={this.onDashboardTypeChange}/>
-                  </Col>
-                </Row>
-              </div>
-            )}/>
+            <Route
+              exact
+              path="/"
+              render={props => (
+                <div>
+                  <Row>
+                    <Col s={12}>
+                      <Header />
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col s={12}>
+                      <PitchHeader />
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col s={12}>
+                      <Features />
+                    </Col>
+                  </Row>
+                </div>
+              )}
+            />
+            <Route
+              exact
+              path="/dashboards"
+              render={props => <Adaptive screenSize={screenSize} />}
+            />
+            <Route
+              exact
+              path="/help"
+              render={props => (
+                <div>
+                  <Row>
+                    <Col s={12}>
+                      <Header />
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col s={12}>
+                      <Help_Title />
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col s={12}>
+                      <Help_Page
+                        onDashboardTypeChange={this.onDashboardTypeChange}
+                      />
+                    </Col>
+                  </Row>
+                </div>
+              )}
+            />
             <Row>
               <Col s={12}>
                 <Footer />
---
+++ src/components/Adaptive.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Adaptive = ({screenSize}) => {
+
+    return (
+      <div>
+        {screenSize}
+      </div>
+    );
+}
+
+export default Adaptive;
