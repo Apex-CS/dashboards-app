8e0cf938f95b5394bbeec77e1f7c40f3203875dd
Edgar Muñoz <edgarmunoz@localhost.localdomain>
2018-06-19 10:34:05 -0500

    fixing navItem.js

--- src/components/common/DashboardsHeader.jsx
+++ src/components/common/DashboardsHeader.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Navbar, Icon } from 'react-materialize';
 import NavItem from './NavItem';
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom';
 import logo from '../../assets/images/intersys-logo.svg';
 import '../../assets/css/header.css';
 
