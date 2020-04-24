4be767dc3ba520a31c26837f42667cc194813df5
Marco Antonio Loyo <v-mloyo@vrbo.com>
2020-04-23 20:14:14 -0500

    Change intersys logo to apex logo

--- src/components/common/MainHeader.jsx
+++ src/components/common/MainHeader.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Navbar, Icon } from 'react-materialize';
 import NavItem from './NavItem';
 import { Link } from 'react-router-dom';
-import logo from '../../assets/images/intersys-logo.svg';
+import logo from '../../assets/images/apex-logo.jpg';
 import '../../assets/css/header.css';
 
 class Header extends Component {
